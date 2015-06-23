71fbf0a103285d02accd44c40eaf628c199172b1
lishiyi <lishiyiiii@gmail.com>
2015-06-23 17:17:53 -0400

    Initalize

---
+++ src/edu/nyu/lishiyi/Subsets.java
@@ -0,0 +1,46 @@
+package edu.nyu.lishiyi;
+
+import java.util.ArrayList;
+
+/**
+ * Subsets
+ * @author Administrator
+ * Given a set of distinct integers, return all possible subsets.
+
+Have you met this question in a real interview? Yes
+Example
+If S = [1,2,3], a solution is:
+
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+Note
+Elements in a subset must be in non-descending order.
+
+The solution set must not contain duplicate subsets.
+ */
+
+public class Subsets {
+    /**
+     * @param S: A set of numbers.
+     * @return: A list of lists. All valid subsets.
+     */
+    public ArrayList<ArrayList<Integer>> subsets(ArrayList<Integer> S) {
+        // write your code here
+    	
+    	return null;
+    }
+    
+    
+    private void sebsetHelper(ArrayList<Integer> path, int[] num, int pos){
+    	
+    	
+    }
+}
