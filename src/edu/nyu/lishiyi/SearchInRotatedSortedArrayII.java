9c09351c50ddd9d8984cd402e8950fab44178687
lishiyi <lishiyiiii@gmail.com>
2015-06-26 17:54:36 -0400

    Search in Rotated Sorted Array II

---
+++ src/edu/nyu/lishiyi/SearchInRotatedSortedArrayII.java
@@ -0,0 +1,18 @@
+package edu.nyu.lishiyi;
+/**
+ * Search in Rotated Sorted Array II
+ * @author lishiyi
+ *
+ */
+public class SearchInRotatedSortedArrayII {
+
+    /** 
+     * @param A : an integer rotated sorted array and duplicates are allowed
+     * @param target :  an integer to be search
+     * @return : a boolean 
+     */
+    public boolean search(int[] A, int target) {
+        // write your code here
+    	return false;
+    }
+}
