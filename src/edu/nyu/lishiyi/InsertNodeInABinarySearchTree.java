1dabe52a949b75de54fea34e12095602207a3703
lishiyi <lishiyiiii@gmail.com>
2015-07-15 17:13:50 -0400

    Insert Node in a Binary Search Tree

---
+++ src/edu/nyu/lishiyi/InsertNodeInABinarySearchTree.java
@@ -0,0 +1,53 @@
+/*
+Insert Node in a Binary Search Tree
+
+42% Accepted
+Given a binary search tree and a new tree node, insert the node into the tree. You should keep the tree still be a valid binary search tree.
+
+Have you met this question in a real interview? Yes
+Example
+Given binary search tree as follow, after Insert node 6, the tree should be:
+
+  2             2
+ / \           / \
+1   4   -->   1   4
+   /             / \ 
+  3             3   6
+*/
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root: The root of the binary search tree.
+     * @param node: insert this node into the binary search tree
+     * @return: The root of the new binary search tree.
+     */
+    public TreeNode insertNode(TreeNode root, TreeNode node) {
+        // write your code here
+
+        if(root == null){
+
+            return node; 
+        }
+
+        if(node.val < root.val){
+
+            root.left = insertNode(root.left, node);
+        }
+        else{
+
+            root.right = insertNode(root.right, node);
+        }
+
+        return root;
+}
