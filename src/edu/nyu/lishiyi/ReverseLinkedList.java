763d0fb557945b79ddcc82616ac8e2389929b0bf
lishiyi <lishiyiiii@gmail.com>
2015-07-20 16:05:15 -0400

    Reverse Linked List

---
+++ src/edu/nyu/lishiyi/ReverseLinkedList.java
@@ -0,0 +1,30 @@
+Reverse Linked List
+
+41% Accepted
+Reverse a linked list.
+
+Have you met this question in a real interview? Yes
+Example
+For linked list 1->2->3, the reversed linked list is 3->2->1
+
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param head: The head of linked list.
+     * @return: The new head of reversed linked list.
+     */
+    public ListNode reverse(ListNode head) {
+        // write your code here
+    }
+}
+
