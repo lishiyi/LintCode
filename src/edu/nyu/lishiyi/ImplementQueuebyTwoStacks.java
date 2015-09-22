8c85420b67c8ecb82ba8aed0bee1121b1515e30b
lishiyi <lishiyiiii@gmail.com>
2015-09-22 14:46:37 -0400

    Implement Queue by Two Stacks

---
+++ src/edu/nyu/lishiyi/ImplementQueuebyTwoStacks.java
@@ -0,0 +1,55 @@
+Implement Queue by Two Stacks
+
+41% Accepted
+As the title described, you should only use two stacks to implement a queue's actions.   '
+
+The queue should support push(element), pop() and top() where pop is pop the first(a.k.a front) 
+element in the queue.
+
+Both pop and top methods should return the value of first element.
+
+Have you met this question in a real interview? Yes
+Example
+For push(1), pop(), push(2), push(3), top(), pop(), you should return 1, 2 and 2
+
+Challenge
+implement it by two stacks, do not use any other data structure and push, pop and top should be O(1) 
+by AVERAGE.
+
+public class Solution {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    public Solution() {
+       // do initialization if necessary
+    	stack1 = new Stack<Integer>();
+    	stack2 = new Stack<Integer>();
+    }
+    
+    public void push(int element) {
+        // write your code here
+        stack1.push(element);
+    }
+
+    public int pop() {
+        // write your code here
+        if(stack2.empty()){
+        	while(!stack1.empty()){
+
+        		stack2.push(stack1.pop());
+        	}
+        }
+        return stack2.pop();
+    }
+
+    public int top() {
+        // write your code here
+        if(stack2.empty()){
+        	while(!stack1.empty()){
+
+        		stack2.push(stack1.pop());
+        	}
+        }
+        return stack2.peek();
+    }
+}
