2ee10eea244529de0ffcbf5ef3926b9ed78edb4d
lishiyi <lishiyiiii@gmail.com>
2015-09-25 19:11:19 -0400

    add comment

--- src/edu/nyu/lishiyi/Median.java
+++ src/edu/nyu/lishiyi/Median.java
@@ -23,17 +23,24 @@ public class Solution {
      */
     public int median(int[] nums) {
         // write your code here
+
+        //1. Create a reveseOrder() compatator, to make head be max 
+        //(In PriorityQueue: By default, the head is min) 
         Comparator<Integer> revCmp = Collections.reverseOrder();
         Queue<Integer> leftHeap = new PriorityQueue<Integer>(20, revCmp);
         Queue<Integer> rightHeap = new PriorityQueue<Integer>();
 
+        //2. Iterate all the nums
         for(int num : nums){
-
+        	//3. Add it to the rightHeap first.
         	rightHeap.add(num);
+        	//4. If right has 2 numbers more than left, move the smallest in right to left
         	if(rightHeap.size() > leftHeap.size() + 1){
 
         		leftHeap.add(rightHeap.poll());
         	}
+        	//5. If right has 1 number more than left, compare the max of left and min of right,
+        	//   if >, swap them.
         	else if(!leftHeap.isEmpty() && rightHeap.size() == leftHeap.size() + 1){
 
         		if(leftHeap.peek() > rightHeap.peek()){
@@ -43,7 +50,6 @@ public class Solution {
         		}
         	}
         }
-
         if(leftHeap.size() == rightHeap.size()){
 
         	return leftHeap.peek();
