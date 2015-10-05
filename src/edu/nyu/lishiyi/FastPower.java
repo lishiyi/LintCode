3c6f38a3d9df371a0d68267bd9eb7ef66d6b5581
lishiyi <lishiyiiii@gmail.com>
2015-10-05 15:30:04 -0400

    Fast Power

---
+++ src/edu/nyu/lishiyi/FastPower.java
@@ -0,0 +1,36 @@
+Fast Power
+
+19% Accepted
+Calculate the an % b where a, b and n are all 32bit integers.
+
+Have you met this question in a real interview? Yes
+Example
+For 231 % 3 = 2
+
+For 1001000 % 1000 = 0
+
+Challenge
+O(logn)
+
+class Solution {
+    /*
+     * @param a, b, n: 32bit integers
+     * @return: An integer
+     */
+    public int fastPower(int a, int b, int n) {
+        // write your code here
+        if(n == 1){
+        	return a % b;
+        }
+        if(n == 0){
+        	return 1 % b;
+        }
+
+        long product = fastPower(a, b, n / 2);
+        product = product * product % b;
+        if(n % 2 == 1){
+        	product *= a % b;
+        }
+        return (int) product;
+    }
+};
