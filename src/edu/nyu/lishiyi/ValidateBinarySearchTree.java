46c4b85ecc06222eb12a6b6fe83569230b5c1f02
lishiyi <lishiyiiii@gmail.com>
2015-10-25 18:19:45 -0400

    VBST

--- src/edu/nyu/lishiyi/ValidateBinarySearchTree.java
+++ src/edu/nyu/lishiyi/ValidateBinarySearchTree.java
@@ -39,15 +39,41 @@ public class Solution {
      * @param root: The root of binary tree.
      * @return: True if the binary tree is BST, or false
      */
+    private class ResultType{
+
+        boolean isBST;
+        int maxVal;
+        int minVal;
+        public ResultType(boolean isBST, int maxVal, int minVal){
+          this.isBST = isBST;
+          this.maxVal = maxVal;
+          this.minVal = minVal;
+        }
+    }
+
     public boolean isValidBST(TreeNode root) {
         // write your code here
+        ResultType result = helper(root);
+        return result.isBST;
+    }
+
+    private ResultType helper(TreeNode root){
+
         if(root == null){
 
-        	return true;
+            return new ResultType(true, Integer.MIN_VALUE, Integer.MAX_VALUE);
         }
 
-        
-        return isValidBST(root.left) && isValidBST(root.right) &&
-        	   root.left.val < root.val && root.val < root.right.val;
+        ResultType left = helper(root.left); 
+        ResultType right = helper(root.right);
+
+        if(!left.isBST || !right.isBST){
+            return new ResultType(false, 0, 0);
+        }
+        if(root.left != null && left.maxVal > root.val
+          || root.right != null && right.minVal < root.val){
+          return new ResultType(false, 0, 0);
+        }
+        return new ResultType(true, Math.max(root.val, right.maxVal), Math.min(root.val, left.minVal));
     }
 }
